c42e4ae7c6e94f9ce4aae7c95fd924f833bc4bae
Deepak Kumar Verma <boostyourselfup@gmail.com>
2023-06-28 17:11:42 +0530

    Fourth Question solved

---
+++ q2_scaffold/src/App.js
@@ -0,0 +1,15 @@
+import About from "./About";
+import Hero from "./Hero";
+import Skills from "./Skills";
+import "./styles.css";
+
+export default function App() {
+  // Code here
+  return(
+    <>
+      <Hero />
+      <Skills />
+      <About />
+    </>
+  )
+}
---
+++ q2_scaffold/src/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <p>Name: Pranav Sharad Yeole</p>
+        <p>Email: pranav@google.com</p>
+        <p>Phone: 8546465544</p>
+        <p>Address: ABC, xyz street.</p>
+    </div>
+  )
+}
+
+export default Hero
